539e655465d68145627e281eeaeed6bb72d0d9ec
BlbStred <a.dan.brand@gmail.com>
2026-04-15 21:10:29 -1000

    Added class Hand

--- play.py
+++ play.py
@@ -11,19 +11,47 @@ class Card:
     numCards = numSuits * numDenom
     
 
-    def __init__(self, index):  # index into all cards
+    def __init__(self, key):  # index into all cards
 
-        self.suit  = index // Card.numDenom
-        self.denom = index  % Card.numDenom        
+        self.suit  = key // Card.numDenom
+        self.denom = key  % Card.numDenom
+
+
+    def getKey(self):
+        return self.suit * Card.numDenom + self.denom
 
 
     def __str__(self):
         return '[' + str(self.denom) + ['C', 'D', 'H', 'S'][self.suit] + ']'
 
+
     
+
+class Hand:   # of cards
+
+    def __init__(self):
+        self.cards = []
+
+    def add(self, card):
+        self.cards.append(card)
+        self.cards.sort(key = Card.getKey)   # sort to reduce variety of states
+
+
+    def __str__(self):
+        result = ""
+        for c in self.cards:
+            result = result + " " + str(c)
+        return result
+            
         
 if __name__ == "__main__":
-    deck = [Card(i) for i in range(Card.numCards)]
 
+    
+    deck = [Card(Card.numCards - i - 1) for i in range(Card.numCards)]
+
+    hand = Hand()
+    
     for d in deck:
-        print(d)
+        hand.add(d)
+        print("added", d, "to", hand)
+        
