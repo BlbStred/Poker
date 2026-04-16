6011cfaf4312677244b86e3e4c19de12269573d3
BlbStred <a.dan.brand@gmail.com>
2026-04-15 19:16:02 -1000

    Created class Card

--- play.py
+++ play.py
@@ -4,5 +4,26 @@ import torch.optim as optim
 from torch.distributions import Categorical
 
 
+class Card:
+
+    numSuits = 2
+    numDenom = 3
+    numCards = numSuits * numDenom
+    
+
+    def __init__(self, index):  # index into all cards
+
+        self.suit  = index // Card.numDenom
+        self.denom = index  % Card.numDenom        
+
+
+    def __str__(self):
+        return '[' + str(self.denom) + ['C', 'D', 'H', 'S'][self.suit] + ']'
+
+    
+        
 if __name__ == "__main__":
-    print("Hello")
+    deck = [Card(i) for i in range(Card.numCards)]
+
+    for d in deck:
+        print(d)
